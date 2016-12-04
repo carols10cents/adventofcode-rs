438896ec78b2d91406b8d6ece804297ad9c0b629
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-04 15:22:22 -0500

    Create a binary that reads a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::open("./src/input.txt")
+                        .expect("Could not open input.txt");
+    let mut input = String::new();
+    file.read_to_string(&mut input).expect("Could not read input.txt");
 }
