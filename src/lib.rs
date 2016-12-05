dd8ab0eade2ca843394563e747af88d8adb6d86f
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-04 23:39:39 -0500

    Actually test the method I was trying to test
    And with a more meaningful test case too.

--- src/lib.rs
+++ src/lib.rs
@@ -113,10 +113,10 @@ mod test {
 
     #[test]
     fn test_five_most_common_chars() {
-        let room = Room::new("aaaaa-bbb-z-y-x-123[abxyz]");
+        let room = Room::new("not-a-real-room-404[oarel]");
         assert_eq!(
-            room.chars_by_frequency_desc(),
-            vec!['a', 'b', 'x', 'y', 'z']
+            room.five_most_common_chars(),
+            vec!['o', 'a', 'r', 'e', 'l']
         );
     }
 
