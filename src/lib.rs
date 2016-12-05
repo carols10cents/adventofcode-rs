8c4faee5de4763743a1a8a2edaba723f3355e183
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-04 21:40:38 -0500

    A first silly test

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,17 @@ pub fn puzzle(input: &str) -> u32 {
     0
 }
 
+pub fn is_a_triangle(a: u32, b: u32, c: u32) -> bool {
+    true
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn equilateral_triangle() {
+        assert!(is_a_triangle(3, 3, 3));
+    }
 
 }
