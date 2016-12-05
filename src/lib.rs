3f78adf9ab0f0ba3e705eb95d07c6e1e26bd8553
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-05 18:48:52 -0500

    Tried to use md5, decided to let the compiler tell me types

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+extern crate md5;
 
 pub fn puzzle(door_id: &str) -> String {
     let mut answer = String::new();
@@ -5,13 +6,23 @@ pub fn puzzle(door_id: &str) -> String {
 
     for _ in 0..9 {
         let (new_index, next_char) = next_interesting_md5(door_id, index);
-        index = new_index;
+        index = new_index + 1;
         answer.push(next_char);
     }
     answer
 }
 
-pub fn next_interesting_md5(door_id: &str, index: u32) -> (u32, char) {
+pub fn next_interesting_md5(door_id: &str, mut index: u32) -> (u32, char) {
+    loop {
+        let candidate = format!("{}{}", door_id, index);
+        let digest = md5::compute(candidate);
+        if digest.starts_with("00000") {
+            return (index, digest.char_at(5))
+        } else {
+            index += 1;
+        }
+    }
+
     (0, 'a')
 }
 
