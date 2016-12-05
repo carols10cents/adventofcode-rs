0fdef28c16c117859410b7ee29f10f7704b6c9f4
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-04 19:57:12 -0500

    Setup for day 2

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,6 @@ fn main() {
                         .expect("Could not open input.txt");
     let mut input = String::new();
     file.read_to_string(&mut input).expect("Could not read input.txt");
-    let answer = advent::distance(&input);
+    let answer = advent::puzzle(&input);
     println!("The answer is {}", answer);
 }
