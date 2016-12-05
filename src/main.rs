2b229c58797a928998b652d19b5de762ff22d86e
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-04 21:37:28 -0500

    Setup for day 3

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,12 @@ use std::io::prelude::*;
 
 extern crate advent;
 
-use advent::Keypad;
-
 fn main() {
-    let keypad_input = [
-        "  1  ",
-        " 234 ",
-        "56789",
-        " ABC ",
-        "  D  ",
-    ].join("\n");
-    let mut keypad = Keypad::new(&keypad_input);
-
     let mut file = File::open("./src/input.txt")
                         .expect("Could not open input.txt");
     let mut input = String::new();
     file.read_to_string(&mut input).expect("Could not read input.txt");
 
-    let answer = advent::puzzle(&mut keypad, &input);
+    let answer = advent::puzzle(&input);
     println!("The answer is {}", answer);
 }
