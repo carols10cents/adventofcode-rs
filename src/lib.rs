3759d0d463a443cd30a3a2adb090ae3c994d2525
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-04 21:47:10 -0500

    Get the false case working

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub fn puzzle(input: &str) -> u32 {
 }
 
 pub fn is_a_triangle(a: u32, b: u32, c: u32) -> bool {
-    true
+    a + b > c && b + c > a && c + a > b
 }
 
 #[cfg(test)]
@@ -16,4 +16,13 @@ mod test {
         assert!(is_a_triangle(3, 3, 3));
     }
 
+    #[test]
+    fn not_a_triangle() {
+        assert!( ! is_a_triangle(5, 10, 25) );
+        assert!( ! is_a_triangle(5, 25, 10) );
+        assert!( ! is_a_triangle(10, 5, 25) );
+        assert!( ! is_a_triangle(10, 25, 5) );
+        assert!( ! is_a_triangle(25, 5, 10) );
+        assert!( ! is_a_triangle(25, 10, 5) );
+    }
 }
