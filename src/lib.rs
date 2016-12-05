8523cc310a11670d236b1f00a2a056b5ccc9c4af
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-05 18:40:39 -0500

    Start top-down with the code I wish I had

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,14 @@
 
-pub fn puzzle(input: &str) -> String {
-    String::from("nope")
+pub fn puzzle(door_id: &str) -> String {
+    let answer = String::new();
+    let mut index = 0;
+
+    for _ in 0..9 {
+        let (new_index, next_char) = next_interesting_md5(door_id, index);
+        index = new_index;
+        answer.push(next_char);
+    }
+    answer
 }
 
 #[cfg(test)]
