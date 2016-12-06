a7e74315cb293691f8b3a634ef6de9b9b22f4716
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-05 19:02:35 -0500

    Will this test ever pass? Add some printing and... nope

--- src/lib.rs
+++ src/lib.rs
@@ -14,14 +14,15 @@ pub fn puzzle(door_id: &str) -> String {
 
 pub fn next_interesting_md5(door_id: &str, mut index: u32) -> (u32, char) {
     loop {
+        if index % 1000000 == 0 {
+            println!("at index {}", index);
+        }
         let candidate = format!("{}{}", door_id, index);
 
-        let digest = String::from_utf8(
-            md5::compute(candidate.as_bytes()).into_iter().cloned().collect()
-        ).expect("creating string failed");
+        let digest = md5::compute(candidate.as_bytes());
 
-        if digest.starts_with("00000") {
-            return (index, digest.chars().nth(5).expect("no 5th char"))
+        if digest.starts_with(b"00000") {
+            return (index, *digest.iter().nth(5).expect("no 5th char") as char)
         } else {
             index += 1;
         }
@@ -31,5 +32,10 @@ pub fn next_interesting_md5(door_id: &str, mut index: u32) -> (u32, char) {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn once() {
+        assert_eq!(next_interesting_md5("abc", 0), (3231929, '1'));
+    }
 
 }
