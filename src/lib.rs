1986c75e09f0b30eb9d0b5e131946c2da2b907f5
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-06 20:21:18 -0500

    Ok, processing the input into cols and cols into answer
    Seems straightforward enough.
    
    The answer I get tho is qrqlznrl. Something tells me that's not right.
    Oh wow it is right. Okay.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
 use std::collections::HashMap;
 
 pub fn puzzle(input: &str) -> String {
-    String::from("no")
+    let mut lines = input.lines();
+    let first_line = lines.next().expect("There were no lines!");
+
+    let mut columns: Vec<Column> = first_line.chars().map(|ch| {
+        let mut c = Column { freqs: HashMap::new() };
+        c.add_char(ch);
+        c
+    }).collect();
+
+    for line in lines {
+        for (i, ch) in line.chars().enumerate() {
+            columns[i].add_char(ch);
+        }
+    }
+
+    let mut answer = String::new();
+    for col in columns {
+        answer.push(col.most_common_char())
+    }
+    answer
 }
 
 pub struct Column {
@@ -33,5 +52,27 @@ mod test {
         c.add_char('b');
         c.add_char('a');
         assert_eq!(c.most_common_char(), 'a');
+    }
+
+    #[test]
+    fn sample() {
+        let input = "\
+eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar";
+        assert_eq!(puzzle(input), String::from("easter"));
     }
 }
