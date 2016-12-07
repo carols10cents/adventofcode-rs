3957f8a928b12670bd2787540da0c999c78bf9fb
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-06 20:13:18 -0500

    Write a struct with methods for representing a column
    Does not compile

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,36 @@
+use std::collections::HashMap;
+
 pub fn puzzle(input: &str) -> String {
     String::from("no")
+}
+
+struct Column {
+    freqs: HashMap<char, u32>,
+}
+
+impl Column {
+    fn add_char(&mut self, ch: char) {
+        let count = self.freqs.entry(ch).or_insert(0);
+        *count += 1;
+    }
+
+    fn most_common_char(&self) -> char {
+        let mut f: Vec<char, u32> = self.freqs.iter().collect();
+        f.sort_by(|a, b| b.1.cmp(&a.1));
+        f[0]
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
+    #[test]
+    fn most_common_char_seen_in_column() {
+        let mut c = Column { freqs: HashMap::new() };
+        c.add_char('a');
+        c.add_char('b');
+        c.add_char('a');
+        assert_eq!(c.most_common_char(), 'a');
+    }
 }
