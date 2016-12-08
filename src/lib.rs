56ea14d5edb0664e06dc8f822f06038bca7dd1a4
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-08 18:06:55 -0500

    screen struct with new fn

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,29 @@ pub fn puzzle(input: &str) -> u32 {
     0
 }
 
+#[derive(Debug, PartialEq)]
+pub struct Screen {
+    display: Vec<Vec<char>>,
+}
+
+impl Screen {
+    pub fn new(width: usize, height: usize) -> Screen {
+        Screen {
+            display: vec![vec!['.'; width]; height],
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn new_screen(){
+        let s = Screen::new(1, 2);
+        assert_eq!(s, Screen {
+            display: vec![vec!['.'], vec!['.']],
+        });
+    }
 
 }
