8f3c8c6d42a0716dbad363b70df9fa9f8854d14d
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-08 18:41:31 -0500

    Implement rotate column

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,24 @@ impl Screen {
             display: vec![vec!['.'; width]; height],
         }
     }
+
+    pub fn rect(&mut self, rect_width: usize, rect_height: usize) {
+        for h in 0..rect_height {
+            for w in 0..rect_width {
+                self.display[h][w] = '#';
+            }
+        }
+    }
+
+    pub fn rotate_column(&mut self, col_index: usize, how_many: usize) {
+        let col: Vec<_> = self.display.iter()
+            .map(|row| row[col_index] ).collect();
+
+        for i in 0..self.display.len() {
+            let from_index = (i + self.display.len() - how_many) % self.display.len();
+            self.display[i][col_index] = col[from_index];
+        }
+    }
 }
 
 impl fmt::Display for Screen {
@@ -39,6 +57,21 @@ mod test {
             display: vec![vec!['.'], vec!['.']],
         });
         assert_eq!(s.to_string(), ".\n.\n");
+    }
+
+    #[test]
+    fn rect_drawing() {
+        let mut s = Screen::new(7, 3);
+        s.rect(3, 2);
+        assert_eq!(s.to_string(), "###....\n###....\n.......\n");
+    }
+
+    #[test]
+    fn rotate_column() {
+        let mut s = Screen::new(7, 3);
+        s.rect(3, 2);
+        s.rotate_column(1, 1);
+        assert_eq!(s.to_string(), "#.#....\n###....\n.#.....\n");
     }
 
 }
