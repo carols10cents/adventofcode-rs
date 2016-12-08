aa87c69af10b25321c724a12ac80698767edf632
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-07 19:07:51 -0500

    Try to make a method that can say a string has an ABBA or not
    This panics because I'm going out of bounds 😂

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,32 @@ pub fn puzzle(input: &str) -> u32 {
     0
 }
 
+pub fn contains_abba(candidate: &str) -> bool {
+    let by_chars: Vec<char> = candidate.chars().collect();
+
+    for (i, &c) in by_chars.iter().enumerate() {
+        if c != by_chars[i + 1] &&
+            by_chars[i + 1] == by_chars[i + 2] &&
+            by_chars[i + 3] == c {
+            return true
+        }
+    }
+    false
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn does_contain_abba() {
+        assert!(contains_abba("abba"));
+        assert!(contains_abba("ioxxoj"));
+    }
+
+    #[test]
+    fn does_not_contain_abba() {
+        assert!( ! contains_abba("mnop") );
+        assert!( ! contains_abba("aaaa") );
+    }
 }
