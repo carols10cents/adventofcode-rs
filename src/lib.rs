b7447cc882401dfacc438f542612147b11a1c495
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-09 17:13:42 -0500

    Make some failing tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 
 pub fn puzzle(input: &str) -> usize {
+    decompressed_length(input)
+}
+
+pub fn decompressed_length(input: &str) -> usize {
     0
 }
 
@@ -7,4 +11,31 @@ pub fn puzzle(input: &str) -> usize {
 mod test {
     use super::*;
 
+    #[test]
+    fn no_change() {
+        assert_eq!(decompressed_length("ADVENT"), 6);
+    }
+
+    #[test]
+    fn one_char() {
+        assert_eq!(decompressed_length("A(1x5)BC"), 7);
+    }
+
+    #[test]
+    fn three_chars() {
+        assert_eq!(decompressed_length("(3x3)XYZ"), 9);
+    }
+
+    #[test]
+    fn two_chars_twice() {
+        assert_eq!(decompressed_length("A(2x2)BCD(2x2)EFG"), 11);
+    }
+    #[test]
+    fn no_double_processing() {
+        assert_eq!(decompressed_length("(6x1)(1x3)A"), 6);
+    }
+    #[test]
+    fn another_no_double_processing() {
+        assert_eq!(decompressed_length("X(8x2)(3x3)ABCY"), 18);
+    }
 }
