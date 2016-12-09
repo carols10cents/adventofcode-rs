8f35140fa4ba796b468d21d011a42981fcf21ff1
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-09 17:31:48 -0500

    Update the tests for part 2

--- src/lib.rs
+++ src/lib.rs
@@ -58,15 +58,17 @@ mod test {
     }
 
     #[test]
-    fn two_chars_twice() {
-        assert_eq!(decompressed_length("A(2x2)BCD(2x2)EFG"), 11);
+    fn decompressed_twice() {
+        assert_eq!(decompressed_length("X(8x2)(3x3)ABCY"), 20);
     }
+
     #[test]
-    fn no_double_processing() {
-        assert_eq!(decompressed_length("(6x1)(1x3)A"), 6);
+    fn lots_of_a() {
+        assert_eq!(decompressed_length("(27x12)(20x12)(13x14)(7x10)(1x12)A"), 241920);
     }
+
     #[test]
-    fn another_no_double_processing() {
-        assert_eq!(decompressed_length("X(8x2)(3x3)ABCY"), 18);
+    fn variety() {
+        assert_eq!(decompressed_length("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"), 445);
     }
 }
