87ef5f61d8e5371491cce3234763c044d54d2a79
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-10 16:04:13 -0500

    Start a bot struct

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,31 @@ pub fn puzzle(input: &str) -> usize {
     0
 }
 
+pub struct Bot {
+    chip1: Option<usize>,
+    chip2: Option<usize>,
+}
+
+impl Bot {
+    pub fn new() -> Bot {
+        Bot { chip1: None, chip2: None }
+    }
+
+    pub fn has_two_chips(&self) -> bool {
+        match (self.chip1, self.chip2) {
+            (Some(_), Some(_)) => true,
+            _ => false,
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
+    #[test]
+    fn new_bot_does_not_have_two_chips() {
+        let bot = Bot::new();
+        assert!( ! bot.has_two_chips() );
+    }
 }
