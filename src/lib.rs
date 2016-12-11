7b375eb2e0cb28a55c97150a9d964967a6ba6618
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-11 16:11:02 -0500

    Set up a BFS queue

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,20 @@
 
 pub fn puzzle(input: &str) -> usize {
-    0
+    let initial_world_state = WorldState {
+
+    };
+    let mut queue = vec![initial_world_state];
+    while !queue.is_empty() {
+        let world = queue.remove(0);
+
+    }
+
+    panic!("Exhausted all possible moves without finding end condition!");
+
+}
+
+pub struct WorldState {
+
 }
 
 #[cfg(test)]
