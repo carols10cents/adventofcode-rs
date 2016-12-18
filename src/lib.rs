fb96c1e83cc005af40b46242562425b5d6915a98
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-18 18:18:10 -0500

    Simplest case

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,37 @@ pub fn puzzle(input: &str) -> u32 {
     0
 }
 
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum Tile {
+    Safe,
+    Trap,
+}
+
+fn next_tile(left: Tile, center: Tile, right: Tile) -> Tile {
+    Tile::Safe
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
+    #[test]
+    fn safe() {
+        assert_eq!(next_tile(Tile::Safe, Tile::Safe, Tile::Safe), Tile::Safe);
+    }
+
 }
+//
+// ...
+// ..^
+// .^.
+// ^..
+// .^^
+// ^.^
+// ^^.
+// ^^^
+//
+// Its left and center tiles are traps, but its right tile is not.
+// Its center and right tiles are traps, but its left tile is not.
+// Only its left tile is a trap.
+// Only its right tile is a trap.
