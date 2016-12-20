1eed439ba250ed1c1347996b90b0ed38d2b299db
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2016-12-19 21:38:29 -0500

    Remove half of the elves each round, instead of one by one
    This runs in a reasonable amount of time.
    1841611 is right!

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,59 @@
 pub fn puzzle(input: usize) -> usize {
-    let mut circle = vec![1; input];
-    let mut current_elf = 0;
+    let mut circle: Vec<_> = (1..(input + 1)).collect();
+    let mut start_at_0 = true;
 
-    loop {
+    while circle.len() > 1 {
 
-        let next_elf_with_presents = circle[current_elf + 1..].iter()
-            .position(|&num| num != 0);
-        match next_elf_with_presents {
-            Some(i) => {
-                let absolute_next_elf = i + current_elf + 1;
-                circle[current_elf] += circle[absolute_next_elf];
-                circle[absolute_next_elf] = 0;
+        let orig_len = circle.len();
+
+        let next_circle: Vec<_> = match (circle.len() % 2 == 0, start_at_0) {
+            (_, true)  => {
+                circle.into_iter()
+                      .enumerate()
+                      .filter(|&(i, _)| i % 2 == 0)
+                      .map(|(_, val)| val).collect()
             },
-            None => {
-                let next_elf_with_presents = circle[0..current_elf].iter()
-                    .position(|&num| num != 0);
-
-                match next_elf_with_presents {
-                    Some(i) => {
-                        circle[current_elf] += circle[i];
-                        circle[i] = 0;
-                    },
-                    None => return current_elf + 1,
-                }
+            (_, _)  => {
+                circle.into_iter()
+                      .enumerate()
+                      .filter(|&(i, _)| i % 2 != 0)
+                      .map(|(_, val)| val).collect()
             },
+        };
+
+        let started_even_length = orig_len % 2 == 0;
+        let started_odd_length = !started_even_length;
+
+        if start_at_0 && started_even_length {
+            start_at_0 = true;
+        } else if start_at_0 && started_odd_length {
+            start_at_0 = false;
+        } else if !start_at_0 && started_odd_length {
+            start_at_0 = true;
+        } else { // !start_at_0 && started_even_length
+            start_at_0 = false;
         }
-
-        match circle[current_elf + 1..].iter().position(|&num| num != 0) {
-            Some(i) => current_elf = i,
-            None => {
-                let next_elf_with_presents = circle[0..current_elf].iter()
-                    .position(|&num| num != 0);
-
-                match next_elf_with_presents {
-                    Some(i) => current_elf = i,
-                    None => panic!("something has gone horribly wrong"),
-                }
-            },
-        }
+        circle = next_circle;
     }
+    circle.pop().expect("horrible things")
 }
+
+// start_at_0 = false
+// vec![1, 2, 3, 4] -
+// remove even, 0-based positions
+// start_at_0 = false
+
+// vec![1, 2, 3, 4, 5, 6] - len().is_even() && start == 0 => remove odd, 0-based positions
+// vec![1, 3, 5] - len().is_odd() && start == 0 => remove odd, 0-based positions
+// vec![1, 5] - len().is_even() && start == end => remove even, 0-based positions
+// vec![5]
+//
+// vec![1, 2, 3, 4, 5] - len().is_odd() && start == 0 => remove odd, 0-based positions
+// vec![1, 3, 5] len().is_odd() && start == end => remove even, 0-based positions
+// vec![3]
+
+
+
 
 #[cfg(test)]
 mod test {
@@ -49,6 +63,12 @@ mod test {
     fn two_elves() {
         let winner = puzzle(2);
         assert_eq!(winner, 1);
+    }
+
+    #[test]
+    fn five_elves() {
+        let winner = puzzle(5);
+        assert_eq!(winner, 3);
     }
 
 }
